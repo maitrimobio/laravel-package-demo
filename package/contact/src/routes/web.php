b50f588a3712c137afb0de75892b6c1e901fdd12
maitri-shah <maitri.shah@mobiosolutions.com>
2020-02-14 11:58:15 +0530

    Initial commit

---
+++ package/contact/src/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::get('contact', function () {
+    return ['Hello', 'Mobio Solution
+            B/706, Ganesh Plaza,Nr. Navrangpura Bus Stand,
+            Navrangpura, Ahmedabad,Gujarat - 380009. India.'];
+});
